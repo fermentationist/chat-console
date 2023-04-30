7d6b1a994cb269007b3e03cd57ea09387cf5cdb2
Dennis Hodges <fermentationist@gmail.com>
2023-04-30 17:59:59 -0500

    Added nap to woke-dyno

--- server/index.js
+++ server/index.js
@@ -226,8 +226,14 @@ wss.on("close", () => {
 
 httpServer.listen(PORT, () => {
   cLog(`server listening on port ${PORT}`);
+  const offset = 4; // NY
+  const getOffsetHours = (hours) => (hours + offset) > 24 ? 24 - (hours + offset) : hours + offset;
+  const napStartHour = getOffsetHours(22);
+  const napEndHour = getOffsetHours(7)
   wakeDyno({
     url: WAKE_SERVER_URL,
-    interval: WAKE_SERVER_INTERVAL,
+    interval: WAKE_SERVER_INTERVAL, 
+    startNap: [napStartHour, 0, 0, 0],
+    endNap: [napEndHour, 0, 0, 0]
   }).start();
 });
