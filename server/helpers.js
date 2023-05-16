adc7ca009b5164b7e1db29ead6a4de74be792a51
Dennis Hodges <fermentationist@gmail.com>
2023-05-16 09:34:03 -0500

    Removed unused class CancellablePromise from helpers.js

--- server/helpers.js
+++ server/helpers.js
@@ -18,29 +18,3 @@ export const wakeServer = () => {
     // do nothing if fetch fails
   }) : null;
 };
-
-export class CancellablePromise extends Promise {
-  constructor(executor) {
-    let tempResolve, tempReject;
-    super((resolve, reject) => {
-      tempResolve = resolve;
-      tempReject = reject;
-      return executor(resolve, reject);
-    });
-    this.resolve = tempResolve.bind(this);
-    this.reject = tempReject.bind(this);
-    this.fulfill = this.fulfill.bind(this);
-    this.cancel = this.cancel.bind(this);
-    return this;
-  }
-
-  fulfill(value) {
-    console.log("fulfilling promise:", value);
-    return this.resolve(value);
-  }
-
-  cancel(reason = "cancelled") {
-    console.log("cancelling promise...");
-    return this.reject(reason);
-  }
-}
